18d079f9fc76614e416baa65b32f6d8a5b55a4bf
Namcha <Lanend18@gmail.com>
2023-06-19 22:33:17 +0700

    Route

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function (){
+     echo "<h1>Hello</h1>";
+});
+
+Route::get('/users', function (){
+    echo "<h1>Hello Bass</h1>";
+});
